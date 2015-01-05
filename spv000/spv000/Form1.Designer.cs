3152c438c6066cb833a02bd70eeee9f5f9f17f7b
bolatberk <brk.bolat@gmail.com>
2015-01-05 10:54:40 +0200

    basic serialport program
    seri port haberlesme programı

---
+++ spv000/spv000/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace spv000
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaudR = new System.Windows.Forms.ComboBox();
+            this.comboBoxDataB = new System.Windows.Forms.ComboBox();
+            this.comboBoxStopB = new System.Windows.Forms.ComboBox();
+            this.comboBoxParity = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.myserial = new System.IO.Ports.SerialPort(this.components);
+            this.rtbEkran = new System.Windows.Forms.TextBox();
+            this.richTextBoxSend = new System.Windows.Forms.RichTextBox();
+            this.Openport_Click = new System.Windows.Forms.Button();
+            this.closeport_Click = new System.Windows.Forms.Button();
+            this.buttonSend_Click_Click = new System.Windows.Forms.Button();
+            this.clear_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPort.TabIndex = 0;
+            // 
+            // comboBoxBaudR
+            // 
+            this.comboBoxBaudR.FormattingEnabled = true;
+            this.comboBoxBaudR.Items.AddRange(new object[] {
+            "75",
+            "110",
+            "300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaudR.Location = new System.Drawing.Point(12, 39);
+            this.comboBoxBaudR.Name = "comboBoxBaudR";
+            this.comboBoxBaudR.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBaudR.TabIndex = 1;
+            // 
+            // comboBoxDataB
+            // 
+            this.comboBoxDataB.FormattingEnabled = true;
+            this.comboBoxDataB.Items.AddRange(new object[] {
+            "7",
+            "8"});
+            this.comboBoxDataB.Location = new System.Drawing.Point(12, 66);
+            this.comboBoxDataB.Name = "comboBoxDataB";
+            this.comboBoxDataB.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDataB.TabIndex = 2;
+            // 
+            // comboBoxStopB
+            // 
+            this.comboBoxStopB.FormattingEnabled = true;
+            this.comboBoxStopB.Items.AddRange(new object[] {
+            "One",
+            "Two"});
+            this.comboBoxStopB.Location = new System.Drawing.Point(12, 120);
+            this.comboBoxStopB.Name = "comboBoxStopB";
+            this.comboBoxStopB.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxStopB.TabIndex = 4;
+            // 
+            // comboBoxParity
+            // 
+            this.comboBoxParity.FormattingEnabled = true;
+            this.comboBoxParity.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even"});
+            this.comboBoxParity.Location = new System.Drawing.Point(12, 93);
+            this.comboBoxParity.Name = "comboBoxParity";
+            this.comboBoxParity.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxParity.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(154, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(154, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "BaudRate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(154, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(154, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Parity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(154, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Stop Bit";
+            // 
+            // rtbEkran
+            // 
+            this.rtbEkran.AllowDrop = true;
+            this.rtbEkran.Location = new System.Drawing.Point(351, 12);
+            this.rtbEkran.Multiline = true;
+            this.rtbEkran.Name = "rtbEkran";
+            this.rtbEkran.Size = new System.Drawing.Size(368, 139);
+            this.rtbEkran.TabIndex = 10;
+            // 
+            // richTextBoxSend
+            // 
+            this.richTextBoxSend.Location = new System.Drawing.Point(351, 169);
+            this.richTextBoxSend.Name = "richTextBoxSend";
+            this.richTextBoxSend.Size = new System.Drawing.Size(368, 41);
+            this.richTextBoxSend.TabIndex = 11;
+            this.richTextBoxSend.Text = "";
+            // 
+            // Openport_Click
+            // 
+            this.Openport_Click.Location = new System.Drawing.Point(12, 167);
+            this.Openport_Click.Name = "Openport_Click";
+            this.Openport_Click.Size = new System.Drawing.Size(75, 23);
+            this.Openport_Click.TabIndex = 12;
+            this.Openport_Click.Text = "Open";
+            this.Openport_Click.UseVisualStyleBackColor = true;
+            this.Openport_Click.Click += new System.EventHandler(this.Openport_Click_Click);
+            // 
+            // closeport_Click
+            // 
+            this.closeport_Click.Location = new System.Drawing.Point(93, 167);
+            this.closeport_Click.Name = "closeport_Click";
+            this.closeport_Click.Size = new System.Drawing.Size(70, 23);
+            this.closeport_Click.TabIndex = 13;
+            this.closeport_Click.Text = "Close";
+            this.closeport_Click.UseVisualStyleBackColor = true;
+            this.closeport_Click.Click += new System.EventHandler(this.closeport_Click_Click);
+            // 
+            // buttonSend_Click_Click
+            // 
+            this.buttonSend_Click_Click.Location = new System.Drawing.Point(351, 227);
+            this.buttonSend_Click_Click.Name = "buttonSend_Click_Click";
+            this.buttonSend_Click_Click.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend_Click_Click.TabIndex = 14;
+            this.buttonSend_Click_Click.Text = "Send";
+            this.buttonSend_Click_Click.UseVisualStyleBackColor = true;
+            this.buttonSend_Click_Click.Click += new System.EventHandler(this.buttonSend_Click_Click_Click);
+            // 
+            // clear_Click
+            // 
+            this.clear_Click.Location = new System.Drawing.Point(644, 227);
+            this.clear_Click.Name = "clear_Click";
+            this.clear_Click.Size = new System.Drawing.Size(75, 23);
+            this.clear_Click.TabIndex = 15;
+            this.clear_Click.Text = "Clear";
+            this.clear_Click.UseVisualStyleBackColor = true;
+            this.clear_Click.Click += new System.EventHandler(this.clear_Click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(781, 269);
+            this.Controls.Add(this.clear_Click);
+            this.Controls.Add(this.buttonSend_Click_Click);
+            this.Controls.Add(this.closeport_Click);
+            this.Controls.Add(this.Openport_Click);
+            this.Controls.Add(this.richTextBoxSend);
+            this.Controls.Add(this.rtbEkran);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxStopB);
+            this.Controls.Add(this.comboBoxParity);
+            this.Controls.Add(this.comboBoxDataB);
+            this.Controls.Add(this.comboBoxBaudR);
+            this.Controls.Add(this.comboBoxPort);
+            this.Name = "Form1";
+            this.Text = "Basic Serial Port Terminal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.ComboBox comboBoxBaudR;
+        private System.Windows.Forms.ComboBox comboBoxDataB;
+        private System.Windows.Forms.ComboBox comboBoxStopB;
+        private System.Windows.Forms.ComboBox comboBoxParity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.IO.Ports.SerialPort myserial;
+        private System.Windows.Forms.TextBox rtbEkran;
+        private System.Windows.Forms.RichTextBox richTextBoxSend;
+        private System.Windows.Forms.Button Openport_Click;
+        private System.Windows.Forms.Button closeport_Click;
+        private System.Windows.Forms.Button buttonSend_Click_Click;
+        private System.Windows.Forms.Button clear_Click;
+    }
+}
+
---
+++ spv000/spv000/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;  //seri port eklendi
+
+
+namespace spv000
+{
+    public partial class Form1 : Form
+    {
+        string indata = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            getportnames();// uygun comportları bu fonksıyon ıle alıp port adlı comboboxxa basıyorum bununla
+            comboBoxBaudR.SelectedIndex = 6;  //default  set for baudrate-9600
+            comboBoxDataB.SelectedIndex = 1;  //default  set for data bits-8
+            comboBoxParity.SelectedIndex = 0; //default  set for parity -none
+            comboBoxStopB.SelectedIndex = 0;  //default  set for stop bits -one
+        }
+        public void Init()
+        {
+            myserial.PortName = comboBoxPort.Text;                                           //selected name of port
+            myserial.Parity = (Parity)Enum.Parse(typeof(Parity), comboBoxParity.Text);   //selected parity 
+            myserial.StopBits = (StopBits)Enum.Parse(typeof(StopBits), comboBoxStopB.Text);//selected stopbits
+            myserial.DataBits = int.Parse(comboBoxDataB.Text);                           //selected data bits
+            myserial.BaudRate = int.Parse(comboBoxBaudR.Text);                             //selected baudrate
+            myserial.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);//received even handler
+
+
+        }
+        public void getportnames()
+        {
+            string[] portnames = SerialPort.GetPortNames(); //uygun portlar bunula alınıyor strınge aktarılıyor 
+            comboBoxPort.Items.Clear();                     //portname adlı combo boxta bir item varsa onlar sılınıyor 
+
+            foreach (string s in portnames)                 //portcombobox ına bulunan portlar eklenıyor
+            {
+                comboBoxPort.Items.Add(s);
+            }
+
+            if (comboBoxPort.Items.Count > 0)   //port bulundumu dıye ıtem sayısına bakılıyor 
+            {
+                comboBoxPort.SelectedIndex = 0;
+            }
+            else
+            {
+                comboBoxPort.Text = "No COM Port "; //port bulunamadıysa uyarı mesajı veriliyor portcomboda
+            }
+        }/** uygun comportları arama fonksıyonu **/
+
+
+        public void transmit()
+        {
+
+            try
+            {
+                if (myserial.IsOpen)
+                {
+
+                    myserial.Write(richTextBoxSend.Text); //rich box taki veri serial porttan basılıyor 
+                    System.Threading.Thread.Sleep(150);// sistem gecikme
+
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("com port uygun degil!!!"); //port uygun deilse uyarı mesaji basiliyor
+            }
+        }/*******************veri basma islemi bitiyor******************/
+
+        public void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+            
+
+            try
+            {
+                indata = myserial.ReadLine();
+                this.Invoke(new EventHandler(DisplayText));
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("Hata:" + hata.Message);
+            }
+            
+        }/*********************************************************/
+
+
+        private void DisplayText(object s, EventArgs e)
+        {
+
+            // grafik verilerinde gelen verinin basında daıma $ isareti bulancak
+            // dolar ısareti bulunmayan datalar text olarak algılancak ve text box ta basılacak bunu algılamak ıcın
+            //Substring ile okunan data nın ilk harfi kontrol ediliyor detaylı kullanım ıcın asagıyı ıncele
+            /*
+             
+             string metin = “Bilişim Teknolojileri”;
+               Console.WriteLine(metin.Substring(3)); //işim Teknolojileri
+             
+             * 
+             * burada 3. harften sonra cumleyi kesiyor ve gerisini alıyor. eger 3. harften belli bir sayıda harf alınamak ıstenırse
+             * string metin = “Bilişim Teknolojileri”;
+                Console.WriteLine(metin.Substring(3,4)); //işim    
+             * olarak kullan burada 3. harften kesımp 4 5 6 7 harleri alıyor yanı 4 harfi
+             * 
+             
+             */
+
+            rtbEkran.Text += indata;
+            rtbEkran.SelectionStart = rtbEkran.Text.Length;
+            rtbEkran.ScrollToCaret();
+            indata = String.Empty;
+        }
+
+        private void Openport_Click_Click(object sender, EventArgs e)
+        {
+            if (myserial.IsOpen)  //com port acıkmı deıl mı dıye bakar
+            {
+                MessageBox.Show("com port zaten acik!!!");
+            }
+            else
+            {
+                Init();
+                myserial.Open();
+                //status.BackColor = Color.Green;
+            }
+
+        }
+
+        private void closeport_Click_Click(object sender, EventArgs e)
+        {
+            if (myserial.IsOpen)
+            {
+                myserial.Close();
+                //status.BackColor = Color.Red;
+            }
+            else
+            {
+                MessageBox.Show("com port zaten kapali!!!");
+            }
+        }
+
+        private void clear_Click_Click(object sender, EventArgs e)
+        {
+            // alma ve gonderme boxları siliniyor.
+            richTextBoxSend.Clear();
+            rtbEkran.Clear();
+        }
+
+        private void buttonSend_Click_Click_Click(object sender, EventArgs e)
+        {
+            transmit();
+        }
+
+
+
+    }
+}
