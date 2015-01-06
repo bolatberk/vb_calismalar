8f2b0172dcdfbc3a2c6b5167a6177651de6ea6b2
bolatberk <brk.bolat@gmail.com>
2015-01-06 19:02:51 +0200

    arayüze ekran kaplama özelliği eklendi

--- serialport_datagraph/spv000/Form1.Designer.cs
+++ serialport_datagraph/spv000/Form1.Designer.cs
@@ -300,6 +300,9 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
@@ -323,6 +326,7 @@
             // 
             // status
             // 
+            this.status.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.status.AutoSize = true;
             this.status.BackColor = System.Drawing.Color.Red;
             this.status.Location = new System.Drawing.Point(695, 12);
@@ -361,6 +365,7 @@
             // 
             // label13
             // 
+            this.label13.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label13.Location = new System.Drawing.Point(6, 271);
@@ -457,6 +462,9 @@
             // 
             // zedGraphControl1
             // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.zedGraphControl1.BackColor = System.Drawing.Color.Transparent;
             this.zedGraphControl1.Cursor = System.Windows.Forms.Cursors.Default;
             this.zedGraphControl1.Location = new System.Drawing.Point(164, 5);
